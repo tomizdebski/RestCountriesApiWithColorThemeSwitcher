e7860533991514cfb379e9f15c94eb36036f9eb9
Tomasz Izdebski <lukas4096@gmail.com>
2023-06-03 06:47:34 +0200

    nxt

--- main.js
+++ main.js
@@ -23,14 +23,31 @@ const getCountry = (url) => {
             const card = document.createElement("div");
             card.className = "card";
 
-            card.innerHTML = `
-                
-                <img class="card__image" src=${el.flags.svg} alt="flag">
-                <h3 class="card__name">${el.name.common}</h3>
-                <p class="desc card__population">Population: ${el.population}</p>
-                <p class="desc card__region">Region: ${el.region}</p>
-                <p class="desc card__capital">Capital: ${el.capital}</p>
-            `;
+            const img = document.createElement("img");
+            img.className = "card__image";
+            img.src = `${el.flags.svg}`;
+            card.appendChild(img);
+
+            const h3 = document.createElement("h3");
+            h3.className = "card__name";
+            h3.innerText = `${el.name.common}`;
+            card.appendChild(h3);
+
+            const paragrafPop = document.createElement("p");
+            paragrafPop.className = "desc card__population";
+            paragrafPop.innerText = `${el.population}`;
+            card.appendChild(paragrafPop);
+
+            const paragrafReg = document.createElement("p");
+            paragrafReg.className = "desc card__region";
+            paragrafReg.innerText = `${el.region}`;
+            card.appendChild(paragrafReg);
+
+            const paragrafCap = document.createElement("p");
+            paragrafCap.className = "desc card__capital";
+            paragrafCap.innerText = `${el.capital}`;
+            card.appendChild(paragrafCap);
+
             cardContainer.appendChild(card);
         });
     });
@@ -50,11 +67,15 @@ selectRegion.addEventListener('change', (e) => {
 
 const cardContainer = document.querySelector(".card_container");
 cardContainer.addEventListener('click', (e) => {
+    const containerSF = document.querySelector(".containerSF");
+    containerSF.innerHTML = `<button class="button button-back"><- Back</button>`;
     console.log(e.target.outerHTML);
     cardContainer.innerHTML='';
     const card = document.createElement('div');
     card.className = "card-big";
     card.innerHTML = `${e.target.outerHTML}`;
+
+
     cardContainer.appendChild(card);
 });
 
@@ -66,7 +87,49 @@ searchInput.addEventListener('change', (e) => {
 });
 
 toggleDarkLight.addEventListener('click', (e) => {
+    const header = document.querySelector(".header");
+    const toggleLightDark = document.querySelector(".header__toggle");
+    const cardContainer = document.querySelector('.card_container');
+    const card = document.querySelectorAll('.card');
+    const contSF = document.querySelector('.containerSF');
+    const search = document.querySelector('.containerSF__input');
+    const filter = document.querySelector('.containerSF__option');
+    const buttonBack = document.querySelector('.button-back');
+    console.log(buttonBack);
+ 
+    
+    if (modeDarkLight === "light") {
+        modeDarkLight = "dark";
+        toggleDarkLight.innerHTML = `<ion-icon  name="moon"></ion-icon>Dark mode`;
+        header.classList.add("light");
+        toggleLightDark.classList.add("light");
+        cardContainer.classList.add("light-card_container");
+        card.forEach(element => {
+            element.classList.add("light");
+        });
+        contSF.classList.add("light-card_container");
+        if(search) search.classList.add("light-card");
+        if(filter) filter.classList.add("light-card");
+        buttonBack.classList.add("light");
 
+
+
+    }else if (modeDarkLight === "dark") {
+        modeDarkLight = "light"
+        toggleDarkLight.innerHTML = `<ion-icon name="sunny"></ion-icon>Light mode`;
+        header.classList.remove("light");
+        toggleLightDark.classList.remove("light");
+        cardContainer.classList.remove("light-card_container");
+        card.forEach(element => {
+            element.classList.remove("light");
+        });
+        contSF.classList.remove("light-card_container");
+        if(search) search.classList.remove("light-card");
+        filter ? filter.classList.remove("light-card"): null;
+        buttonBack.classList.remove("light");
+
+    };
+    
 });
 
 
